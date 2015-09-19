4f35b5b3a6c860672984d405b8e68807b7b1ea9e
kmb32123 <breyk@uwplatt.edu>
2015-09-19 11:12:38 -0500

    style(*): bootstrapify

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -2,20 +2,18 @@ import json
 import os
 import subprocess
 from queue import Queue
-from bottle import route, run, Bottle, request
+from bottle import route, run, Bottle, request, static_file
 from threading import Thread
 
 app = Bottle()
 
 @app.route('/youtube-dl')
 def dl_queue_list():
-    print("bar")
-    return '''
-        <form action="/youtube-dl/q" method="POST">
-            URL: <input name="url" type="text" />
-            <input value="Submit" type="submit" />
-        </form>
-    '''
+    return static_file('index.html', root='./')
+
+@app.route('/youtube-dl/static/:filename#.*#')
+def server_static(filename):
+    return static_file(filename, root='./static')
 
 @app.route('/youtube-dl/q', method='GET')
 def q_size():
@@ -30,19 +28,19 @@ def q_put():
         return { "success" : True, "url" : url }
     else:
         return { "success" : False, "error" : "dl called without a url" }
-        
+
 def dl_worker():
     while not done:
-        item = dl_q.get() 
+        item = dl_q.get()
         download(item)
         dl_q.task_done()
-        
+
 def download(url):
     print("Starting download of " + url)
     command = """youtube-dl -o "/youtube-dl/.incomplete/%(title)s.%(ext)s" -f best[acodec=none][ext=mp4]+best[vcodec=none][ext=m4a] --exec 'touch {} && mv {} /youtube-dl/' --merge-output-format mp4 """ + url
-    print("Finished downloading " + url)
     subprocess.call(command, shell=True)
-    
+    print("Finished downloading " + url)
+
 dl_q = Queue();
 done = False;
 dl_thread = Thread(target=dl_worker)
