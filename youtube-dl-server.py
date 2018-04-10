2bd6d45e087545cf72720c782d82814ef9534ad3
Kevin Brey <manbearwiz@gmail.com>
2018-04-10 01:32:27 -0500

    fix: syntax error

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -37,7 +37,7 @@ def dl_worker():
 
 def download(url):
     print("Starting download of " + url)
-    subprocess.run(["youtube-dl", "-l", "/youtube-dl/.incomplete/%(title)s.%(ext)s", "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", "--exec", "touch {} && mv {} /youtube-dl/", "--merge-output-format", "mp4", url])
+    subprocess.run(["youtube-dl", "-o", "/youtube-dl/.incomplete/%(title)s.%(ext)s", "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", "--exec", "touch {} && mv {} /youtube-dl/", "--merge-output-format", "mp4", url])
     print("Finished downloading " + url)
 
 dl_q = Queue();
