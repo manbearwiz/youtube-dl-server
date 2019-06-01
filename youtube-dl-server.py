1febdc677e44ffa98f0f97c23a466cbfaec4e960
Swicegood <jguru108@gmail.com>
2019-05-31 21:55:56 -0400

    fix datetime picker and passing of options

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -17,7 +17,7 @@ app_defaults = {
     'YDL_EXTRACT_AUDIO_FORMAT': None,
     'YDL_EXTRACT_AUDIO_QUALITY': '192',
     'YDL_RECODE_VIDEO_FORMAT': None,
-    'YDL_OUTPUT_TEMPLATE': '/youtube-dl/%(title)s [%(id)s].%(ext)s',
+    'YDL_OUTPUT_TEMPLATE': '/Users/jaga/Public/youtube-dl/%(title)s [%(id)s].%(ext)s',
     'YDL_ARCHIVE_FILE': None,
     'YDL_SERVER_HOST': '0.0.0.0',
     'YDL_SERVER_PORT': 8080,
@@ -94,7 +94,9 @@ def get_ydl_options(request_options):
             'preferedformat': ydl_vars['YDL_RECODE_VIDEO_FORMAT'],
         })
     
-    date_range = request_options.get('daterange')	
+    raw_date_range = request_options.get('daterange')
+    start, end = raw_date_range.split(" - ")
+    date_range = youtube_dl.DateRange(start,end)
     
     return {
 	'daterange': date_range,
