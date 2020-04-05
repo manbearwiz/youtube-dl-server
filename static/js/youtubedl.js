d35fbc61d2f5cad37f67aeb39556411169cf030b
Maxence <max@23.tf>
2020-04-05 03:53:16 +0000

    Add escaped url to confirmation message

--- static/js/youtubedl.js
+++ static/js/youtubedl.js
@@ -1,8 +1,25 @@
+var entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
+
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+    return entityMap[s];
+  });
+}
+
 function set_dismissible_message(result_data){
   success = result_data.success;
   message_list = $('#message_list').html();
   message_list += "<div class=\"alert alert-" + (success ? "success" : "danger")+ " alert-dismissible fade show\" role=\"alert\">";
-  message_list += "<strong>" + (success ? "Success" : "Error") + "</strong>: " + (success ? "Link added to the list" : result_data.error) +".";
+  message_list += "<strong>" + (success ? "Success" : "Error") + "</strong>: " + (success ? (escapeHtml($("#url").val()) + " added to the list") : result_data.error) +".";
   message_list += "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>";
   message_list += "</div>";
   $("#message_list").html(message_list);
