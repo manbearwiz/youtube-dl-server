6a3e8992326ae6ca3e3883f49d2a90d747dda2ad
Maxence <max@23.tf>
2020-04-15 02:43:48 +0000

    Clean pop up message display in js

--- static/js/youtubedl.js
+++ static/js/youtubedl.js
@@ -15,10 +15,9 @@ function escapeHtml(string) {
   });
 }
 
-function set_dismissible_message(result_data){
-  success = result_data.success;
+function set_dismissible_message(success, message){
   message_list = "<div class=\"alert alert-" + (success ? "success" : "danger")+ " alert-dismissible fade show\" role=\"alert\">";
-  message_list += "<strong>" + (success ? "Success" : "Error") + "</strong>: " + (success ? (escapeHtml($("#url").val()) + " added to the list") : result_data.error) +".";
+  message_list += "<strong>" + (success ? "Success" : "Error") + "</strong>: " + message;
   message_list += "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>";
   message_list += "</div>";
   message_list += $('#message_list').html();
@@ -29,11 +28,11 @@ function submit_video(){
   data = {url: $("#url").val(),format: $("#format").val()};
   $.post("/api/downloads", data)
     .done(function (data) {
-      set_dismissible_message(data);
+      set_dismissible_message(data.success, data.success ? (escapeHtml($("#url").val()) + " added to the list.") : data.error);
       $("#url").val("");
     })
     .fail(function() {
-      set_dismissible_message({'success': false, 'error': 'Could not add the url to the queue'});
+      set_dismissible_message(false, 'Could not add the url to the queue.');
   });
 }
 
@@ -73,8 +72,7 @@ function show_logs_detail(){
 }
 
 function toggle_hide_logs_detail(){
-  console.log($("td:nth-child(5)").is(":visible"))
-  if ($("td:nth-child(5)").is(":visible")) {
+  if ($("th:nth-child(5)").is(":visible")) {
     hide_logs_detail();
   }
   else {
