b414220bb388b90d4ec9002edf9dc415b99d8869
Maxence <max@23.tf>
2020-05-19 00:14:51 +0000

    Update DB schema.
    Passing this commit will reset the log database

--- ydl_server/logdb.py
+++ ydl_server/logdb.py
@@ -10,13 +10,19 @@ STATUS_NAME =["Running",
         "Failed",
         "Pending"]
 
-
 class Actions:
     DOWNLOAD = 1
     PURGE_LOGS = 2
     INSERT = 3
     UPDATE = 4
     RESUME = 5
+    SET_NAME = 6
+    SET_STATUS = 7
+    SET_LOG = 8
+
+class JobType:
+    YDL_DOWNLOAD = 0
+    YDL_UPDATE = 1
 
 class Job:
     RUNNING = 0
@@ -24,13 +30,15 @@ class Job:
     FAILED = 2
     PENDING = 3
 
-    def __init__(self, name, status, log, format):
-        self.id = -1
+    def __init__(self, name, status, log, jobtype, format=None, url=None, id=-1):
+        self.id = id
         self.name = name
         self.status = status
         self.log = log
         self.last_update = ""
         self.format = format
+        self.type = jobtype
+        self.url = url
 
     @staticmethod
     def clean_logs(logs):
@@ -46,14 +54,30 @@ class Job:
 class JobsDB:
 
     @staticmethod
+    def check_db_latest():
+        conn = sqlite3.connect("file://%s" % app_defaults['YDL_DB_PATH'], uri=True)
+        cursor = conn.cursor()
+        cursor.execute("PRAGMA table_info('jobs')")
+        columns = [row[1] for row in cursor.fetchall()]
+        if set(columns) != set(['id', 'name', 'status', 'format', 'log', 'last_update', 'type', 'url']):
+            print("Outdated jbos table, cleaning up and recreating")
+            cursor.execute("DROP TABLE jobs;")
+        conn.close()
+
+
+    @staticmethod
     def init_db():
         conn = sqlite3.connect("file://%s" % app_defaults['YDL_DB_PATH'], uri=True)
         cursor = conn.cursor()
-        cursor.execute("CREATE TABLE if not exists jobs (id INTEGER PRIMARY KEY \
-                AUTOINCREMENT, name TEXT NOT NULL, \
-                status INTEGER NOT NULL, log TEXT, \
-                format TEXT NOT NULL, \
-                last_update DATETIME DEFAULT CURRENT_TIMESTAMP);")
+        cursor.execute("CREATE TABLE if not exists jobs \
+                (id INTEGER PRIMARY KEY AUTOINCREMENT, \
+                name TEXT NOT NULL, \
+                status INTEGER NOT NULL, \
+                log TEXT, \
+                format TEXT, \
+                last_update DATETIME DEFAULT CURRENT_TIMESTAMP, \
+                type INTEGER NOT NULL, \
+                url TEXT);")
         conn.commit()
         conn.close()
 
@@ -62,13 +86,16 @@ class JobsDB:
                                             "?mode=ro" if readonly else ""),
                                     uri=True)
 
+
     def close(self):
         self.conn.close()
 
     def insert_job(self, job):
         cursor = self.conn.cursor()
-        cursor.execute("INSERT INTO jobs (name, status, log, format) VALUES (?,\
-                ?, ?, ?);", (job.name, str(job.status), job.log, job.format))
+        cursor.execute("INSERT INTO jobs (name, status, log, format, type, \
+                url) VALUES (?, ?, ?, ?, ?, ?);",
+                (job.name, str(job.status), job.log, job.format, str(job.type),
+                    job.url))
         job.id = cursor.lastrowid
         self.conn.commit()
 
@@ -78,6 +105,24 @@ class JobsDB:
                 where id = ?;", (str(job.status), job.log, str(job.id)))
         self.conn.commit()
 
+    def set_job_status(self, job_id, status):
+        cursor = self.conn.cursor()
+        cursor.execute("UPDATE jobs SET status = ?, last_update = datetime() \
+                where id = ?;", (str(status), str(job_id)))
+        self.conn.commit()
+
+    def set_job_log(self, job_id, log):
+        cursor = self.conn.cursor()
+        cursor.execute("UPDATE jobs SET log = ?, last_update = datetime() \
+                where id = ?;", (log, str(job_id)))
+        self.conn.commit()
+
+    def set_job_name(self, job_id, name):
+        cursor = self.conn.cursor()
+        cursor.execute("UPDATE jobs SET name = ?, last_update = datetime() \
+                where id = ?;", (name, str(job_id)))
+        self.conn.commit()
+
     def purge_jobs(self):
         cursor = self.conn.cursor()
         cursor.execute("DELETE FROM jobs;")
@@ -85,14 +130,15 @@ class JobsDB:
 
     def get_all(self, limit=50):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT id, name, status, log, last_update, format from jobs ORDER BY last_update DESC LIMIT ?;", (str(limit),))
+        cursor.execute("SELECT id, name, status, log, last_update, format, type, url from jobs ORDER BY last_update DESC LIMIT ?;", (str(limit),))
         rows = []
-        for job_id, name, status, log, last_update, format in cursor.fetchall():
+        for job_id, name, status, log, last_update, format, jobtype, url in cursor.fetchall():
             rows.append({'id': job_id,
                         'name': name,
                         'status': STATUS_NAME[status],
                         'log': log,
                         'format': format,
-                        'last_update': last_update})
+                        'last_update': last_update,
+                        'type': jobtype,
+                        'url': url})
         return rows
-
