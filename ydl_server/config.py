9267f59221bc048301afe120d3303936fedf6cf9
Maxence <max@23.tf>
2020-08-03 23:21:04 +0000

    Add YDL_RAW_OPTIONS config variable to pass ydl vars
    YDL_RAW_OPTIONS is a dict that can be set to pass raw parameters to
    youtube-dl.
    In the default config, ignoreerrors has been added and set to True as a
    default element for YDL_RAW_OPTIONS. This allows playslist downloads to
    go on if an error is encountered on a video.

--- ydl_server/config.py
+++ ydl_server/config.py
@@ -11,5 +11,8 @@ app_defaults = {
     'YDL_CACHE_DIR': '/youtube-dl/.cache',
     'YDL_DB_PATH': '/youtube-dl/.ydl-metadata.db',
     'YDL_SUBTITLES_LANGUAGES': None,
-    'YDL_DEBUG': False
+    'YDL_DEBUG': False,
+    'YDL_RAW_OPTIONS': {
+        'ignoreerrors': True
+        },
 }
--- ydl_server/ydlhandler.py
+++ ydl_server/ydlhandler.py
@@ -106,6 +106,8 @@ def get_ydl_options(request_options):
         'cachedir': ydl_vars['YDL_CACHE_DIR']
     }
 
+    ydl_options = {**ydl_vars['YDL_RAW_OPTIONS'], **ydl_options}
+
     if ydl_vars['YDL_SUBTITLES_LANGUAGES']:
         ydl_options['writesubtitles'] = True
         if ydl_vars['YDL_SUBTITLES_LANGUAGES'] != 'all':
