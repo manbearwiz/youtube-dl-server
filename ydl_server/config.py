cc9c6a1c590886f337303289be59b1430d97553e
Maxence <max@23.tf>
2020-11-16 04:41:29 +0000

    Update configuration file validation

--- ydl_server/config.py
+++ ydl_server/config.py
@@ -4,12 +4,16 @@ import yaml
 def load_config(config_path):
     config = None
     if not os.path.isfile(config_path):
-        print("Config file '%s' not found." % config_path)
+        print("Config file `{}` not found.".format(config_path))
         return None
     with open(config_path) as configfile:
         config = yaml.load(configfile, Loader=yaml.SafeLoader)
     return config
 
-app_config = load_config(os.environ.get('YDL_CONFIG_PATH', 'config.yml'))
-if app_config is None or app_config.get('ydl_server') is None:
-    raise Exception('No configuration file found')
+config_path = os.environ.get('YDL_CONFIG_PATH', 'config.yml')
+app_config = load_config(config_path)
+
+if app_config is None or app_config.get('ydl_server') is None or \
+        app_config.get('ydl_options') is None or \
+        app_config['ydl_options'].get('output') is None:
+    raise Exception('Invalid configuration file `{}`'.format(config_path))
