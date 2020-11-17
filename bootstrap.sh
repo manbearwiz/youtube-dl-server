f41765b13ef2920b97043929dfd615be1d634d3e
Maxence <max@23.tf>
2020-11-17 04:07:58 +0000

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 
 # Download required web frontend libraries
-curl -s https://code.jquery.com/jquery-3.4.1.min.js > ydl_server/static/js/jquery.min.js
-curl -s https://unpkg.com/@popperjs/core@2.1.1/dist/umd/popper.min.js > ydl_server/static/js/popper.min.js
+wget -q https://code.jquery.com/jquery-3.4.1.min.js -O ydl_server/static/js/jquery.min.js
+wget -q https://unpkg.com/@popperjs/core@2.1.1/dist/umd/popper.min.js -O ydl_server/static/js/popper.min.js
 wget -q https://github.com/twbs/bootstrap/releases/download/v4.4.1/bootstrap-4.4.1-dist.zip
-unzip -j bootstrap-4.4.1-dist.zip *.css *.css.map -d ydl_server/static/css/
-unzip -j bootstrap-4.4.1-dist.zip *.js *.js.map -d ydl_server/static/js/
+unzip -jo bootstrap-4.4.1-dist.zip *.css *.css.map -d ydl_server/static/css/
+unzip -jo bootstrap-4.4.1-dist.zip *.js *.js.map -d ydl_server/static/js/
 rm -f bootstrap-4.4.1-dist.zip
