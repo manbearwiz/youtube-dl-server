06393cbe957567e290551303c45d24a5316f36f1
Maxence <max@23.tf>
2023-04-07 13:42:04 +0000

    Fix vue assets base

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
